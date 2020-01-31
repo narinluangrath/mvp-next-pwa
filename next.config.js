a422dead804442a393045961ff70ed9639949777
Narin Luangrath <narin.luangrath@gmail.com>
2020-01-30 16:07:30 -0800

    Get the service worker registered using a custom server

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    ...defaultConfig
-  }
+const withOffline = require('next-offline');
+
+const offlineConfig = {
+  generateInDevMode: true
 }
+
+// module.exports = (phase, { defaultConfig }) => {
+//   return {
+//     ...defaultConfig,
+//     ...withOffline(offlineConfig)
+//   }
+// }
+
+module.exports = withOffline(offlineConfig);
