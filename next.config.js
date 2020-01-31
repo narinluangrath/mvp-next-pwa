2822bcb791cc4a1b0e1ef748434b16fa2e01889d
Narin Luangrath <narin.luangrath@gmail.com>
2020-01-30 16:16:37 -0800

    Delete comment

--- next.config.js
+++ next.config.js
@@ -4,11 +4,4 @@ const offlineConfig = {
   generateInDevMode: true
 }
 
-// module.exports = (phase, { defaultConfig }) => {
-//   return {
-//     ...defaultConfig,
-//     ...withOffline(offlineConfig)
-//   }
-// }
-
 module.exports = withOffline(offlineConfig);
